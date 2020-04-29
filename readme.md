bfe7612c6dedcf8acc1084e013c7701bb74150f7
Anthony <yoskovia@mail.gvsu.edu>
2020-04-29 15:29:49 -0400

    Adding readme

---
+++ readme.md
@@ -0,0 +1,1 @@
+# Helpful / practical / interesting functions in python!
--- reorder.py
+++ reorder.py
@@ -5,11 +5,34 @@ Quickly reordering the columns of a dataframe.
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame({
-    'a':np.random.randint(1, 4, 5),
-    'cost_two':np.random.randint(1, 4, 5),
-    'b':np.random.randint(1, 4, 5),
-    'test_price':np.random.randint(1, 4, 5),
-    'test_thing':np.random.randint(1, 4, 5),
-    'cost_one':np.random.randint(1, 4, 5)
-})
+np.random.seed(15)
+
+rand = lambda : np.random.randint(1, 4, 5)
+cols = ['a', 'cost_two', 'b', 'test_price', 'test_thing', 'cost_one']
+
+df = pd.DataFrame({col : rand() for col in cols})
+
+def reorder(df, *args, everything=True):
+    """ Returns dataframe sorted using lists and functions.
+    
+    Params:
+    df (pd.DataFrame): Input dataframe
+    everything (bool): Whether or not to return all columns
+    *args: Lists of columns or functions that return booleans
+    """
+    
+    print(args)
+    
+    allCols = df.columns
+    
+    # For each arg:
+    # If its a list, append
+    colOrder = [x for x in args if isinstance(x, list)]
+    
+    if everything:
+        return df[args[0]]
+    else:
+        print('Returning just a subset.')
+
+
+display(reorder(df, ['a', 'b'], 'asdf'))
